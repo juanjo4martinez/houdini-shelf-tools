10565639962e78f876b7080e36c4e900f5b649ba
Juanjo Martínez <47528725+juanjo4martinez@users.noreply.github.com>
2022-03-21 13:05:44 -0400

    Fixed compatibility issues in Houdini 19/Python 3
    Unpacked dictionary keys and values into lists. Replaced the old "copysource" parameter by the new "copysourcelayer1".

--- agentBrowser.py
+++ agentBrowser.py
@@ -1,16 +1,17 @@
 '''
 AGENT BROWSER
-Easy way to browse through your characters folder and import them as
-Agents for your Crowd simulation.
+Easy way to browse through your characters and import them as Agents
+for your Crowd simulation.
 
 HOW IT WORKS:
-This tool looks for .FBX files in your Agent Directory and lists them
-in a UI.
+This tool looks for .FBX files in your Agent directory and displays them
+as a list with the help of a UI.
 
-You can then pick one or more characters from that list, and the following
-nodes will be generated for each of them: Agent, Agent Clip, Agent Layer and Agent Prep.
+You can then pick one or more Agents from the list, and the tool will
+automatically set up the nodes for you (Agent, Agent Clip, Agent Layer
+and Agent Prep).
 
-Feel free to change the "agentDir" variable with your own Agent Directory.
+Feel free to change the "agentDir" variable with your own Agent directory.
 
 IMPORTANT:
 This code looks for subfolders too, so if you store your .FBX motion clips
@@ -18,6 +19,10 @@ in the same directory as your .FBX agents you may get some unexpected results.
 
 In order to avoid that, make sure to have ONLY YOUR .FBX CHARACTERS in the
 Agent Directory.
+
+*** If you are using HOUDINI 18.5, replace the "copysourcelayer1" parameter
+in line 127 by "sourcecopy" so that it can be run in the old AGENT LAYER node
+***
 '''
 
 import os
@@ -50,10 +55,17 @@ for path,subdirs,files in os.walk(agentDir):
             filePath = os.path.join(path, file)
            
             dict[agentName] = filePath
+            
+# Unpack the names and paths from the dictionary and store them in lists            
+agentList = []
+[agentList.append(agentName) for agentName in dict.keys()]
+
+fileList = []
+[fileList.append(filePath) for filePath in dict.values()]
 
 # Open a "Select from list" window and ask the user to choose one or more agents
 # NOTE: The output will be a tuple with index(es)
-selectedAgents = hou.ui.selectFromList(dict.keys(),
+selectedAgents = hou.ui.selectFromList(agentList,
                                 title='Agent Browser',
                                 message='Select the character(s) you want to import as agent(s):',
                                 column_header='Agents',
@@ -90,36 +102,36 @@ for index in selectedAgents:
     # Agent node:
     # This will help us determine if we should create the node
     # or update the existing one
-    agentNode = hou.node('{}/{}'.format(agentSetupNode.path(), dict.keys()[index]))
+    agentNode = hou.node('{}/{}'.format(agentSetupNode.path(), agentList[index]))
    
     # If the Agent node doesn't exist, create it
     if not agentNode:
-        agentNode = agentSetupNode.createNode('agent', dict.keys()[index])
+        agentNode = agentSetupNode.createNode('agent', agentList[index])
        
         # Set the Agent Name
         agentNode.parm('agentname').set('$OS')
        
         # Set Input as FBX and the file path
         agentNode.parm('input').set(2)
-        agentNode.parm('fbxfile').set(dict.values()[index])
+        agentNode.parm('fbxfile').set(fileList[index])
         agentNode.parm('fbxclipname').set('tpose')
    
         # Create an Agent Clip node and connect it to the Agent node
-        agentClipNode = agentSetupNode.createNode('agentclip::2.0', '{}_clips'.format(dict.keys()[index]))
+        agentClipNode = agentSetupNode.createNode('agentclip::2.0', '{}_clips'.format(agentList[index]))
         agentClipNode.setInput(0, agentNode)
 
         # Create an Agent Layer node, connect it to the Agent Clip node
         # and activate the Source Layer checkbox so we can see the character
-        agentLayerNode = agentSetupNode.createNode('agentlayer', '{}_layer'.format(dict.keys()[index]))
+        agentLayerNode = agentSetupNode.createNode('agentlayer', '{}_layer'.format(agentList[index]))
         agentLayerNode.setInput(0, agentClipNode)
-        agentLayerNode.parm('sourcecopy').set(1)
+        agentLayerNode.parm('copysourcelayer1').set(1)
 
         # Create an Agent Prep node and connect it to the Agent Layer node
-        agentPrepNode = agentSetupNode.createNode('agentprep::3.0', '{}_prep'.format(dict.keys()[index]))
+        agentPrepNode = agentSetupNode.createNode('agentprep::3.0', '{}_prep'.format(agentList[index]))
         agentPrepNode.setInput(0, agentLayerNode)
 
         # Create an OUT (Null) node and connect it to the Agent Prep node
-        outNode = agentSetupNode.createNode('null', 'OUT_{}'.format(dict.keys()[index]))
+        outNode = agentSetupNode.createNode('null', 'OUT_{}'.format(agentList[index]))
         outNode.setInput(0, agentPrepNode)
         
         # Activate the Display/Render flags and set the color to black
@@ -132,4 +144,4 @@ for index in selectedAgents:
 
     # If the Agent node already exists in the scene, throw a message        
     else:
-        hou.ui.displayMessage('The agent «{}» is already in your scene.'.format(dict.keys()[index]))
+        hou.ui.displayMessage('The agent «{}» is already in your scene.'.format(agentList[index]))
