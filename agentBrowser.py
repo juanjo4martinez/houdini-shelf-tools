2e88a45c10d42bdcccd911999a32a9e22160541b
Juanjo Martínez <47528725+juanjo4martinez@users.noreply.github.com>
2022-07-22 17:21:48 +0200

    Reformat code and update variable names

--- agentBrowser.py
+++ agentBrowser.py
@@ -1,9 +1,11 @@
-'''
-AGENT BROWSER
+"""
+AGENT BROWSER v1.0
+------------------
 Easy way to browse through your characters and import them as Agents
 for your Crowd simulation.
 
-HOW IT WORKS:
+HOW IT WORKS
+------------
 This tool looks for .FBX files in your Agent directory and displays them
 as a list with the help of a UI.
 
@@ -11,137 +13,116 @@ You can then pick one or more Agents from the list, and the tool will
 automatically set up the nodes for you (Agent, Agent Clip, Agent Layer
 and Agent Prep).
 
-Feel free to change the "agentDir" variable with your own Agent directory.
+Feel free to change the 'AGENT_DIR' variable with your own Agent directory.
 
-IMPORTANT:
+IMPORTANT
+---------
 This code looks for subfolders too, so if you store your .FBX motion clips
 in the same directory as your .FBX agents you may get some unexpected results.
 
 In order to avoid that, make sure to have ONLY YOUR .FBX CHARACTERS in the
 Agent Directory.
 
-*** If you are using HOUDINI 18.5, replace the "copysourcelayer1" parameter
-in line 127 by "sourcecopy" so that it can be run in the old AGENT LAYER node
-***
-'''
+* If you are using HOUDINI 18.5, replace the 'copysourcelayer1' parameter
+by 'sourcecopy' so that it can be run in the old AGENT LAYER node.
+"""
 
+# Import built-in modules.
+from collections import OrderedDict
 import os
 import sys
-from collections import OrderedDict
 
+# This is where your Agents are stored.
+AGENT_DIR = "F:/3D/modelos"
 
-############################
-# SEARCHING FOR .FBX FILES #
-############################
+# Dictionary to store Agent names and file paths.
+agents = OrderedDict()
 
-
-# Agent Directory: This is where your Agents are stored
-agentDir = 'C:/Users/14385/3D Objects/characters'
-
-# Initialize dictionary to store Agent names and file paths
-dict = OrderedDict()
-
-# Iterate through the Agent Directory and look at its path, subdirectories and files
-for path,subdirs,files in os.walk(agentDir):
+# Iterate the Agent Directory and get its path, subdirectories and files.
+for path,subdirs,files in os.walk(AGENT_DIR):
    
-    # Iterate through the .FBX files and store their names and paths in the dictionary
+    # Iterate .FBX files and store their names and paths in the dictionary.
     for file in files:
-   
-        if file.endswith('.fbx'):  
-       
-            agentName = file.split(".")
-            agentName = agentName[0]                  
-           
-            filePath = os.path.join(path, file)
-           
-            dict[agentName] = filePath
+        if file.endswith(".fbx"):
+            agent_name = file.split(".")
+            agent_name = agent_name[0]
+            filepath = os.path.join(path, file)
+            agents[agent_name] = filepath
             
-# Unpack the names and paths from the dictionary and store them in lists            
-agentList = []
-[agentList.append(agentName) for agentName in dict.keys()]
+# Unpack names and paths from the dictionary and store them in lists.
+agent_list = []
+[agent_list.append(agent_name) for agent_name in agents.keys()]
 
-fileList = []
-[fileList.append(filePath) for filePath in dict.values()]
+file_list = []
+[file_list.append(filepath) for filepath in agents.values()]
 
-# Open a "Select from list" window and ask the user to choose one or more agents
-# NOTE: The output will be a tuple with index(es)
-selectedAgents = hou.ui.selectFromList(agentList,
-                                title='Agent Browser',
-                                message='Select the character(s) you want to import as agent(s):',
-                                column_header='Agents',
+# Open a "Select from list" window and ask the user to choose one or more agents.
+# NOTE: The output will be a tuple with index(es).
+selected_agents = hou.ui.selectFromList(agent_list,
+                                title="Agent Browser",
+                                message="Select the character(s) you want to import as agent(s):",
+                                column_header="Agents",
                                 width=300)
 
-# If the user doesn't select any agent, stop the program                                
+# If the user doesn't select any agent, stop the program.
 try:
-    selectedAgents[0]
+    selected_agents[0]
 except IndexError:
     sys.exit()
-   
 
-#######################
-# SET UP THE AGENT(S) #
-#######################
-   
-   
-# /obj/ context
-obj = hou.node('/obj/')
+# Get the "agentSetup" node - we'll use this to check if it exists or not.
+obj = hou.node("/obj/")
+geo_node_name = "agentSetup"
+agent_setup_node = hou.node(f"{obj.path()}/{geo_node_name}")
 
-# "agentSetup" node:
-# This will help us determine if we should create the node
-# or update the existing one
-geoNodeName = 'agentSetup'
-agentSetupNode = hou.node('{}/{}'.format(obj.path(), geoNodeName))
-
-# If "agentSetup" doesn't exist, create it
-if not agentSetupNode:
-    agentSetupNode = obj.createNode('geo', geoNodeName)
+# If "agentSetup" doesn't exist, create it.
+if not agent_setup_node:
+    agent_setup_node = obj.createNode("geo", geo_node_name)
    
-# Iterate through every index in the "selectedAgents" tuple
-for index in selectedAgents:
+# Iterate every index in the "selected_agents" tuple.
+for index in selected_agents:
 
-    # Agent node:
-    # This will help us determine if we should create the node
-    # or update the existing one
-    agentNode = hou.node('{}/{}'.format(agentSetupNode.path(), agentList[index]))
-   
-    # If the Agent node doesn't exist, create it
-    if not agentNode:
-        agentNode = agentSetupNode.createNode('agent', agentList[index])
+    # Get the "Agent" node - we'll use this to check if it exists or not.
+    agent_node = hou.node(f"{agent_setup_node.path()}/{agent_list[index]}")
+
+    # If the Agent node doesn't exist, create it.
+    if not agent_node:
+        agent_node = agent_setup_node.createNode("agent", agent_list[index])
        
-        # Set the Agent Name
-        agentNode.parm('agentname').set('$OS')
+        # Set the Agent Name.
+        agent_node.parm("agentname").set("$OS")
        
-        # Set Input as FBX and the file path
-        agentNode.parm('input').set(2)
-        agentNode.parm('fbxfile').set(fileList[index])
-        agentNode.parm('fbxclipname').set('tpose')
+        # Set the Input as FBX and the corresponding file path.
+        agent_node.parm("input").set(2)
+        agent_node.parm("fbxfile").set(file_list[index])
+        agent_node.parm("fbxclipname").set("tpose")
    
-        # Create an Agent Clip node and connect it to the Agent node
-        agentClipNode = agentSetupNode.createNode('agentclip::2.0', '{}_clips'.format(agentList[index]))
-        agentClipNode.setInput(0, agentNode)
+        # Create an Agent Clip node and connect it to the Agent node.
+        agent_clip_node = agent_setup_node.createNode("agentclip", f"{agent_list[index]}_clips")
+        agent_clip_node.setInput(0, agent_node)
 
-        # Create an Agent Layer node, connect it to the Agent Clip node
-        # and activate the Source Layer checkbox so we can see the character
-        agentLayerNode = agentSetupNode.createNode('agentlayer', '{}_layer'.format(agentList[index]))
-        agentLayerNode.setInput(0, agentClipNode)
-        agentLayerNode.parm('copysourcelayer1').set(1)
+        # Create an Agent Layer node, connect it to the Agent Clip node,
+        # and activate the Source Layer checkbox so we can see the character.
+        agent_layer_node = agent_setup_node.createNode("agentlayer", f"{agent_list[index]}_layer")
+        agent_layer_node.setInput(0, agent_clip_node)
+        agent_layer_node.parm("copysourcelayer1").set(1)
 
-        # Create an Agent Prep node and connect it to the Agent Layer node
-        agentPrepNode = agentSetupNode.createNode('agentprep::3.0', '{}_prep'.format(agentList[index]))
-        agentPrepNode.setInput(0, agentLayerNode)
+        # Create an Agent Prep node and connect it to the Agent Layer node.
+        agent_prep_node = agent_setup_node.createNode("agentprep", f"{agent_list[index]}_prep")
+        agent_prep_node.setInput(0, agent_layer_node)
 
-        # Create an OUT (Null) node and connect it to the Agent Prep node
-        outNode = agentSetupNode.createNode('null', 'OUT_{}'.format(agentList[index]))
-        outNode.setInput(0, agentPrepNode)
+        # Create an OUT (Null) node and connect it to the Agent Prep node.
+        out_node = agent_setup_node.createNode("null", f"OUT_{agent_list[index]}")
+        out_node.setInput(0, agent_prep_node)
         
-        # Activate the Display/Render flags and set the color to black
-        outNode.setDisplayFlag(True)
-        outNode.setRenderFlag(True)
-        outNode.setColor(hou.Color((0, 0, 0)))
+        # Activate the Display/Render flags and set the color to black.
+        out_node.setDisplayFlag(True)
+        out_node.setRenderFlag(True)
+        out_node.setColor(hou.Color((0, 0, 0)))
 
-        # Layout nodes inside "agentSetup"
-        agentSetupNode.layoutChildren()
+        # Layout nodes inside "agentSetup".
+        agent_setup_node.layoutChildren()
 
-    # If the Agent node already exists in the scene, throw a message        
+    # If the Agent node already exists in the scene, let the user know.
     else:
-        hou.ui.displayMessage('The agent «{}» is already in your scene.'.format(agentList[index]))
+        hou.ui.displayMessage(f"The agent «{agent_list[index]}» is already in your scene.")
